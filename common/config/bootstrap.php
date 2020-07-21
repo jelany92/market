1e384e96ddb37f4eb99bf3bc45e7d44639d3af3e
jelanyk <sosty.jojty0005@gmail.com>
2020-07-22 00:10:38 +0200

    test

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -2,5 +2,6 @@
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@frontendBook', dirname(dirname(__DIR__)) . '/frontendBook');
+Yii::setAlias('@adamMarket', dirname(dirname(__DIR__)) . '/adamMarket');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
