63a8f24089a9cb870509bb4622856e95d357289f
jelany <j_robben92@hotmail.com>
2020-10-28 12:55:03 +0100

    rtl css

--- backend/views/site/book-gallery.php
+++ backend/views/site/book-gallery.php
@@ -1,11 +1,25 @@
 <?php
 
 use yii\bootstrap4\Html;
+use common\models\DetailGalleryArticle;
+use evgeniyrru\yii2slick\Slick;
 
 /* @var $this yii\web\View */
 /* @var $modelDetailGalleryArticle \common\models\DetailGalleryArticle */
 $this->registerAssetBundle('backend\assets\BookGallery');
+$image = [];
 ?>
+<?php foreach ($modelDetailGalleryArticle as $detailGalleryArticle) : ?>
+    <?php if ($detailGalleryArticle->bookGalleries->book_photo != null) : ?>
+        <?php $image[] = Html::a(Html::img(DetailGalleryArticle::subcategoryImagePath($detailGalleryArticle->bookGalleries->book_photo), [
+            'style' => 'width:100%;height: 300px',
+            'id'    => $detailGalleryArticle->id,
+        ]), [
+                                     'detail-gallery-article/view',
+                                     'id' => $detailGalleryArticle->id,
+                                 ]) ?>
+    <?php endif; ?>
+<?php endforeach; ?>
 <div class="body">
     <!--    <?php /*if (Yii::$app->user->id != 3) : */ ?>
         <?php /*if (Yii::$app->user->id != 2) : */ ?>
@@ -20,15 +34,73 @@ $this->registerAssetBundle('backend\assets\BookGallery');
         <h1><?= Yii::t('app', 'My Library') ?></h1>
     </div>
     <br>
+    <div style="direction: initial">
+        <?= Slick::widget([
+                                                    // Widget configuration. See example above.
+                                                    // settings for js plugin
+                                                    // @see http://kenwheeler.github.io/slick/#settings
+                                                    'clientOptions' => [
+                                                        'dots'           => true,
+                                                        'speed'          => 300,
+                                                        'autoplay'       => true,
+                                                        'infinite'       => true,
+                                                        'slidesToShow'   => 4,
+                                                        'slidesToScroll' => 1,
+                                                        'responsive'     => [
+                                                            [
+                                                                'breakpoint' => 1200,
+                                                                'settings'   => [
+                                                                    'slidesToShow'   => 4,
+                                                                    'slidesToScroll' => 4,
+                                                                    'infinite'       => true,
+                                                                    'autoplay'       => true,
+                                                                    'dots'           => true,
+                                                                ],
+                                                            ],
+                                                            [
+                                                                'breakpoint' => 992,
+                                                                'settings'   => [
+                                                                    'slidesToShow'   => 4,
+                                                                    'slidesToScroll' => 4,
+                                                                    'infinite'       => true,
+                                                                    'autoplay'       => true,
+                                                                    'dots'           => true,
+                                                                ],
+                                                            ],
+                                                            [
+                                                                'breakpoint' => 768,
+                                                                'settings'   => [
+                                                                    'slidesToShow'   => 2,
+                                                                    'slidesToScroll' => 2,
+                                                                    'infinite'       => true,
+                                                                    'autoplay'       => true,
+                                                                    'dots'           => true,
+                                                                ],
+                                                            ],
+                                                            [
+                                                                'breakpoint' => 480,
+                                                                'settings'   => 'unslick',
+                                                                // Destroy carousel, if screen width less than 480px
+                                                            ],
 
+                                                        ],
+                                                    ],
+                                                    'items'         => $image,
+                                                ]); ?>
+
+
+    </div>
     <div class="row">
         <?php foreach ($modelDetailGalleryArticle as $detailGalleryArticle) : ?>
             <div class="books-view col-6 col-sm-3">
                 <?php if ($detailGalleryArticle->bookGalleries->book_photo != null) : ?>
                     <?= Html::a(Html::img(\common\models\DetailGalleryArticle::subcategoryImagePath($detailGalleryArticle->bookGalleries->book_photo), [
                         'style' => 'width:100%;height: 300px',
-                        'id' => $detailGalleryArticle->id,
-                    ]), ['detail-gallery-article/view', 'id' => $detailGalleryArticle->id,]) ?>
+                        'id'    => $detailGalleryArticle->id,
+                    ]), [
+                                    'detail-gallery-article/view',
+                                    'id' => $detailGalleryArticle->id,
+                                ]) ?>
                 <?php else: ?>
                     <?= Html::a('test', 'test', ['style' => "padding-top: 245px;"]) ?>
                 <?php endif; ?>
@@ -45,8 +117,8 @@ $this->registerAssetBundle('backend\assets\BookGallery');
         <?php endforeach; ?>
         <div class="center-block">
             <?= \common\components\LinkPager::widget([
-                'pagination' => $pages,
-            ]); ?>
+                                                         'pagination' => $pages,
+                                                     ]); ?>
         </div>
     </div>
 </div>
